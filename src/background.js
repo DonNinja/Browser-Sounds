37c86ce3c89f4378d5889846282805cff072a2a6
DonNinja <yngvileo@gmail.com>
2023-08-07 21:31:35 +0000

    All function properly

--- src/background.js
+++ src/background.js
@@ -11,42 +11,60 @@ createClosed();
 createActivated();
 
 function createOpen() {
+    let audio = getAudio('open');
+
     // *When windows & tabs open
     browser.windows.onCreated.addListener((window) => {
-        console.log(`New window: ${window.id}`);
+        // console.log(`New window: ${window.id}`);
+        audio.cloneNode(true).play();
     });
 
     browser.tabs.onCreated.addListener((tab) => {
-        console.log(`New tab: ${tab.id}`);
+        // console.log(`New tab: ${tab.id}`);
+        audio.cloneNode(true).play();
     });
 }
 
 function createClosed() {
+    let audio = getAudio('close');
+
     // *When windows & tabs close
     browser.windows.onRemoved.addListener((window) => {
-        console.log(`Closed window: ${window.id}`);
+        // console.log(`Closed window: ${window.id}`);
+        audio.cloneNode(true).play();
     });
 
     browser.tabs.onRemoved.addListener((tab) => {
-        console.log(`Closed tab: ${tab.id}`);
+        // console.log(`Closed tab: ${tab.id}`);
+        audio.cloneNode(true).play();
     });
 }
 
 function createActivated() {
+    // let audio = getAudio('change');
+
+    // *When tab is changed
+    browser.tabs.onActivated.addListener(() => {
+        // audio.cloneNode(true).play();
+    });
+}
+
+function getAudio(filename) {
     let audio = document.createElement('audio');
 
     let source = document.createElement('source');
 
     if (audio.canPlayType('audio/wav')) {
         source.type = 'audio/wav';
-        source.src = '../data/sounds/test.wav';
+        source.src = `../data/sounds/${filename}.wav`;
     }
-    audio.volume = 0.5;
+    if (audio.canPlayType('audio/mp3')) {
+        source.type = 'audio/mp3';
+        source.src = `../data/sounds/${filename}.mp3`;
+    }
+    audio.volume = 0.1;
 
     audio.appendChild(source);
 
-    // *When tab is changed
-    browser.tabs.onActivated.addListener(() => {
-        audio.cloneNode(true).play();
-    });
+    return audio;
 }
