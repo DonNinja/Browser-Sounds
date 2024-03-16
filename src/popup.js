c7cd934cc56c70c0a39f50c250d6b353bd39589f
DonNinja <yngvilth@gmail.com>
2024-03-16 23:41:34 +0000

    Added a text input for volume

--- src/popup.js
+++ src/popup.js
@@ -5,36 +5,11 @@ let defaultVolumes = {
 
 const openSlider = document.getElementById("OpenSlider");
 const closeSlider = document.getElementById("CloseSlider");
-const openValue = document.getElementById("OpenValue");
-const closeValue = document.getElementById("CloseValue");
+// const openValue = document.getElementById("OpenValue");
+// const closeValue = document.getElementById("CloseValue");
+const openInput = document.getElementById("openInput");
+const closeInput = document.getElementById("closeInput");
 const volumeForm = document.getElementById("volumeForm");
-
-function listenForClicks() {
-    document.addEventListener("click", (e) => {
-        /**
-         * Remove the page-hiding CSS from the active tab,
-         * send a "reset" message to the content script in the active tab.
-         */
-        function reset(tabs) {}
-
-        /**
-         * Just log the error to the console.
-         */
-        function reportError(error) {
-            console.error(`Perform: ${error}`);
-        }
-
-        if (e.target.tagName !== "BUTTON" || !e.target.closest("#popupContent")) {
-            // Ignore when click is not on a button within <div id="popupContent">.
-            // return;
-        }
-
-        if (e.target.type === "reset") {
-        }
-        if (e.target.type === "submit") {
-        }
-    });
-}
 
 function resetVolume() {
     const sending = browser.runtime.sendMessage({
@@ -50,10 +25,12 @@ function handleResponse(message) {
         // console.log(`Received volumes: ${defaultVolumes.open}, ${defaultVolumes.close}`);
 
         openSlider.value = defaultVolumes.open;
-        openValue.textContent = defaultVolumes.open;
+        openInput.value = defaultVolumes.open;
+        // openValue.textContent = defaultVolumes.open;
 
         closeSlider.value = defaultVolumes.close;
-        closeValue.textContent = defaultVolumes.close;
+        closeInput.value = defaultVolumes.close;
+        // closeValue.textContent = defaultVolumes.close;
     } else if (message.response === "SUCCESS") {
         console.log(`Successful submit :)`);
     }
@@ -73,28 +50,34 @@ function reportExecuteScriptError(error) {
     console.error(`Failed to execute beastify content script: ${error.message}`);
 }
 
-/**
- * When the popup loads, inject a content script into the active tab,
- * and add a click handler.
- * If we couldn't inject the script, handle the error.
- */
-
-listenForClicks();
-
 openSlider.addEventListener("input", (e) => {
-    openValue.textContent = openSlider.value;
+    openInput.value = openSlider.value;
+    // openValue.textContent = openSlider.value;
 });
 
 closeSlider.addEventListener("input", (e) => {
-    closeValue.textContent = closeSlider.value;
+    // closeValue.textContent = closeSlider.value;
+    closeInput.value = closeSlider.value;
+});
+
+openInput.addEventListener("input", () => {
+    openSlider.value = openInput.value;
+});
+
+closeInput.addEventListener("input", () => {
+    closeSlider.value = closeInput.value;
+});
+
+openInput.addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+        event.preventDefault();
+    }
 });
 
 volumeForm.addEventListener("reset", (event) => {
     event.preventDefault();
     console.log("Sending reset to background");
     resetVolume();
-
-    return true;
 });
 
 volumeForm.addEventListener("submit", (event) => {
@@ -109,8 +92,6 @@ volumeForm.addEventListener("submit", (event) => {
     });
 
     sending.then(resetVolume, handleError);
-
-    return true;
 });
 
 resetVolume();
