ff376837484eeacdb9eef89a6310285770cc6072
Yngvilth <yngvilth@gmail.com>
2025-07-19 12:50:35 +0000

    Started working on making file upload possible

--- src/background.js
+++ src/background.js
@@ -1,20 +1,48 @@
 // 'use strict';
+/* global IDBFiles */
+
+// import { getFileStorage } from ;
 
 // With background scripts you can communicate with popup
 // and contentScript files.
 // For more information on background script,
 // See https://developer.chrome.com/extensions/background_pages
 
+// const BSDB = getFileStorage({ name: "BrowserSounds", persistent: true });
+
 function setItem() {
-    console.log("OK");
+	console.log("OK");
 }
 
 function onError(error) {
-    console.log(error);
+	console.log(error);
 }
 
 console.clear();
 // console.log(`STARTUP`);
+
+const BSDBOpenRequest = indexedDB.open("BrowserSounds");
+
+BSDBOpenRequest.onerror = (e) => {
+	console.error(`Error loading database`);
+}
+
+BSDBOpenRequest.onsuccess = (e) => {
+	console.log(`Database loaded`);
+}
+
+BSDBOpenRequest.onupgradeneeded = (e) => {
+	db = e.target.result;
+
+	db.onerror = (e) => {
+
+	}
+
+	const objectStore = db.createObjectStore('Sounds', { keypath: 'Audio' });
+
+	objectStore.createIndex()
+}
+
 
 createOpen();
 createClosed();
@@ -22,116 +50,116 @@ createClosed();
 let volumes;
 
 async function setVolumes(newVolumes) {
-    await browser.storage.sync
-        .set({
-            volumes: newVolumes,
-        })
-        .then(getVolumes, onError);
+	await browser.storage.sync
+		.set({
+			volumes: newVolumes,
+		})
+		.then(getVolumes, onError);
 }
 
 async function getVolumes() {
-    volumes = (await browser.storage.sync.get("volumes"))["volumes"];
+	volumes = (await browser.storage.sync.get("volumes"))["volumes"];
 
-    // console.log(`New volumes: ${volumes.open} | ${volumes.close}`);
+	// console.log(`New volumes: ${volumes.open} | ${volumes.close}`);
 }
 
 getVolumes().then(
-    () => {
-        return;
-    },
-    () => {
-        if (volumes === undefined) {
-            setVolumes({
-                open: 50,
-                close: 50,
-            });
-        }
-    }
+	() => {
+		return;
+	},
+	() => {
+		if (volumes === undefined) {
+			setVolumes({
+				open: 50,
+				close: 50,
+			});
+		}
+	}
 );
 
 // console.log(volumes);
 
 function createOpen() {
-    let audio = getAudio("open");
+	let audio = getAudio("open");
 
-    browser.tabs.onCreated.addListener((tab) => {
-        let temp = audio.cloneNode(true);
-        temp.volume = volumes.open / 100;
-        temp.play();
-    });
+	browser.tabs.onCreated.addListener((tab) => {
+		let temp = audio.cloneNode(true);
+		temp.volume = volumes.open / 100;
+		temp.play();
+	});
 }
 
 function createClosed() {
-    let audio = getAudio("close");
+	let audio = getAudio("close");
 
-    // *When windows & tabs close
-    browser.windows.onRemoved.addListener((window) => {
-        // console.log(`Closed window: ${window.id}`);
-        // audio.cloneNode(true).play();
-    });
+	// *When windows & tabs close
+	browser.windows.onRemoved.addListener((window) => {
+		// console.log(`Closed window: ${window.id}`);
+		// audio.cloneNode(true).play();
+	});
 
-    browser.tabs.onRemoved.addListener((tab) => {
-        console.log(`Closed tab`);
-        let temp = audio.cloneNode(true);
-        temp.volume = volumes.close / 100;
-        temp.play();
-    });
+	browser.tabs.onRemoved.addListener((tab) => {
+		console.log(`Closed tab`);
+		let temp = audio.cloneNode(true);
+		temp.volume = volumes.close / 100;
+		temp.play();
+	});
 }
 
 function createActivated() {
-    // let audio = getAudio('change');
-    // *When tab is changed
-    // browser.tabs.onActivated.addListener(() => {
-    // audio.cloneNode(true).play();
-    // });
+	// let audio = getAudio('change');
+	// *When tab is changed
+	// browser.tabs.onActivated.addListener(() => {
+	// audio.cloneNode(true).play();
+	// });
 }
 
 function createKeydown() {}
 
 function getAudio(filename) {
-    let audio = document.createElement("audio");
+	let audio = document.createElement("audio");
 
-    let source = document.createElement("source");
+	let source = document.createElement("source");
 
-    if (audio.canPlayType("audio/mp3")) {
-        source.type = "audio/mp3";
-        source.src = `../data/sounds/${filename}.mp3`;
-    }
-    audio.volume = 1;
+	if (audio.canPlayType("audio/mp3")) {
+		source.type = "audio/mp3";
+		source.src = `../data/sounds/${filename}.mp3`;
+	}
+	audio.volume = 1;
 
-    audio.appendChild(source);
+	audio.appendChild(source);
 
-    return audio;
+	return audio;
 }
 
 function handleMessage(request, sender, sendResponse) {
-    console.log(`A content script sent a message: ${request.message}`);
+	console.log(`A content script sent a message: ${request.message}`);
 
-    console.log(request);
+	console.log(request);
 
-    switch (request.message) {
-        case "GET":
-            sendResponse({
-                response: `GET`,
-                currentVolumes: volumes,
-            });
-            break;
+	switch (request.message) {
+		case "GET":
+			sendResponse({
+				response: `GET`,
+				currentVolumes: volumes,
+			});
+			break;
 
-        case "SAVE":
-            let newVolumes = request.newVolumes;
-            setVolumes(newVolumes);
-            sendResponse({
-                response: `SUCCESS`,
-            });
-            break;
+		case "SAVE":
+			let newVolumes = request.newVolumes;
+			setVolumes(newVolumes);
+			sendResponse({
+				response: `SUCCESS`,
+			});
+			break;
 
-        default:
-            console.log();
-            sendResponse({
-                error: "Background does not know about this request.",
-            });
-            break;
-    }
+		default:
+			console.log();
+			sendResponse({
+				error: "Background does not know about this request.",
+			});
+			break;
+	}
 }
 
 browser.runtime.onMessage.addListener(handleMessage);
--- src/contentScript.js
+++ src/contentScript.js
@@ -1,5 +1,5 @@
 "esversion: 11";
-// import YTChapters from "get-youtube-chapters";
+
 
 // Content script file will run in the context of web page.
 // With content script you can manipulate the web pages using
@@ -11,6 +11,7 @@
 
 // For more information on Content Scripts,
 // See https://developer.chrome.com/extensions/content_scripts
+
 
 let styles = `
 
